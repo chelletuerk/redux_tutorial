1c64391bdd5f3b168619f8ba742082218c660ff8
chelle tuerk <michelletuerk@gmail.com>
2017-01-17 11:13:37 -0700

    reducers hooked up

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose } from 'redux'
 import {syncHistoryWithStore } from 'react-router-redux'
-import { browserHistroy } from 'react-router'
+import { browserHistory } from 'react-router'
 
 import rootReducer from './reducers/index'
 import comments from './data/comments'
