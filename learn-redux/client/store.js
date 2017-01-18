d8746a32cb746207cd666f5cc4f32dd514ef29e7
chelle tuerk <michelletuerk@gmail.com>
2017-01-17 17:30:18 -0700

    hooking up redux tools

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -3,6 +3,7 @@ import {syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
 import rootReducer from './reducers/index'
+
 import comments from './data/comments'
 import posts from './data/posts'
 
@@ -11,7 +12,11 @@ const defaultState = {
   comments,
 }
 
-const store = createStore(rootReducer, defaultState)
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
+const store = createStore(rootReducer, defaultState, enhancers)
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
